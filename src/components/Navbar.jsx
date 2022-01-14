dedc3e732e5335547883d0bc96ffb781222ce555
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-01-14 11:43:04 +0530

    code updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Box, Flex, Button, Spacer, Heading, Center } from "@chakra-ui/react"
+import { Box, Flex, Button, Spacer, Heading, Center } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 
 export const Navbar = () => {
+
+    const { isAuth, toggleAuth, setIsLogin } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        toggleAuth();
+        setIsLogin(false);
+    }
 
     return (
         <Flex bg='#F5F5F5'>
@@ -10,7 +19,12 @@ export const Navbar = () => {
             </Box>
             <Spacer />
             <Center mr={6}>
-                <Button colorScheme='teal'>Login</Button>
+                <Button
+                    colorScheme='teal'
+                    onClick={isAuth ? handleLogout : undefined}
+                >
+                    {isAuth ? "Logout" : "Login"}
+                </Button>
             </Center>
         </Flex>
     );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
+import { AuthContextProvider } from './context/AuthContext'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <AuthContextProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
